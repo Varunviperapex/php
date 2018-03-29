d8d78bb47d6d09a3633e45026536f6d72fcf236e
Prahlad Kumar <neowakesup7@gmail.com>
2018-03-29 12:43:11 +0530

    Add files via upload

---
+++ comments_and_assignments.php
@@ -0,0 +1,29 @@
+<?php
+
+$integer =25; //This is a variable
+$integer *= 10;
+
+$string = "Indeed";
+$string .= ".com";
+
+echo $integer.'<br>';
+echo $string;
+
+
+
+/*
+
+$integer += 10;
+$integer =$integer + 10;
+$integer -= 10;
+$integer = $integer - 10;
+$integer /= 10;
+$integer *= 10;
+
+$string = "Indeed";
+$string .= ".com";
+
+*/
+
+
+?>
