919719f77b39d4a45dd77c9a8111477080850159
Prahlad Kumar <neowakesup7@gmail.com>
2018-03-29 12:42:32 +0530

    Add files via upload

---
+++ Arithmetic_operators.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
++ addition
+- substraction
+* multiplication
+/ divide
+% modulus
+-- increment
+++ decrement
+*/
+
+$a =14;
+//$b =7;
+
+$a++;
+//$result = $a % $b;
+echo $a;
+//echo $result;
+
+?>
