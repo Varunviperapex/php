43a34bf5a4ed38fd4e331ba5c38aa04506f90af1
Prahlad Kumar <neowakesup7@gmail.com>
2018-03-30 21:57:21 +0530

    Add files via upload

---
+++ escapechar.php
@@ -0,0 +1,11 @@
+<?php
+
+$stringvar1 ="Hello how are \"you\" doing ";
+$stringvar2 ="Mark";
+$stringvar3 ="today $stringvar2?";
+
+echo $stringvar1 . $stringvar3;
+
+echo " In PHP Escape Sequences:\n Backslash \\ is used as an escaping character.";
+    
+?>
