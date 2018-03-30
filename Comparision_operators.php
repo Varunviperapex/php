526905838795bea19cbde0d32161f94424540ac3
Prahlad Kumar <neowakesup7@gmail.com>
2018-03-30 23:27:13 +0530

    Add files via upload

---
+++ Comparision_operators.php
@@ -0,0 +1,28 @@
+<?php
+
+$name = "fred";
+$num1 = 11;
+
+if($name == "fred")
+{
+echo "The condition is True";
+}
+
+else 
+{
+echo "The condition is False";
+}
+
+
+/*Comparision operators often are used iinside the test condition of conditionals.You can do a comparison against a variable to see if it has a particular value.The values can be either number or strips.
+==
+!= and <>
+>
+<
+>=
+<=
+*/
+
+
+
+?>
