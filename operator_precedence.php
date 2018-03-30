4b6ed5573e35e5a733753d32c5e7bead75b5e39a
Prahlad Kumar <neowakesup7@gmail.com>
2018-03-30 21:57:44 +0530

    Add files via upload

---
+++ operator_precedence.php
@@ -0,0 +1,23 @@
+<?php
+
+//$num = 10 + 5 - 4;
+//$num = 10 + 5 * 4;
+$num = (10 + 5) * 4;
+echo $num;
+
+
+
+
+/* 
+    
+    Default movement is always left to right 
+    
+    +and - are equal in precedence
+    *and / have greater than + and -
+    
+    
+*/
+
+
+
+?>
