22d5b0764f3e1c4082c53a26315197cecea439b0
Prahlad Kumar <neowakesup7@gmail.com>
2018-03-30 23:26:23 +0530

    Add files via upload

---
+++ If_else_statement.php
@@ -0,0 +1,33 @@
+<?php
+
+$num = 2;
+
+if($num > 4)
+{
+echo "konichiwa";
+}
+
+else 
+{
+    echo "Sayonara";
+}
+
+
+/*
+
+
+4 basic parts
+
+1-The if statement
+
+2- Test condition in the parenthesis
+
+3-TRUE and the block of code between the curly braces gets executed
+
+4-False nothing happens (at least for now)
+
+*/
+
+
+
+?>
