e0e9b1d98299ff0d46810c15f74c2c53e32b2610
Prahlad Kumar <neowakesup7@gmail.com>
2018-03-30 23:26:43 +0530

    Add files via upload

---
+++ Else_if_statement.php
@@ -0,0 +1,38 @@
+<?php
+
+$num = 10;
+
+if($num < 5)
+{
+ echo "The condition is true";
+}
+
+elseif($num>=6) 
+
+echo "Elseif was executed";
+
+else 
+{
+echo "The condition is false";
+}
+
+
+
+/*
+
+
+4 basic parts
+
+1-The if statement
+
+2- Test condition in the parenthesis
+
+3-TRUE and the block of code between the curly braces gets executed
+
+4-False nothing happens (at least for now)
+
+*/
+
+
+
+?>
